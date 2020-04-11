86dba70de51ba8830511de072a136afcc7f265f8
fadimata tillman <fadi.tillman@gmail.com>
2020-04-11 23:16:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,6 +65,16 @@ class Student
     end
 
     def self.first_X_students_in_grade_10
+      sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE grade = 10
+        LIMIT ?
+      SQL
+
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
   end
 
 
